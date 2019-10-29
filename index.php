f8c11f68ea72bc40275f6eb88b22609271913101
steroMK <Tautvydas.Strelkauskas@gmail.com>
2019-10-29 15:22:41 +0200

    PHP_Baze

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
-<?php
-print'Veikia!';
-?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>As valdau HTML!</title>
+    <title>Aš valdau HTML!</title>
   </head>
   <body>
-    <h1><b><i>Vardas</b></i> - HTML asas/e!</h1>
-    <p> Moku kurti statini turini. </p>
-
+    <h1><b><i>Vardas</b></i> - HTML asas/ė!</h1>
+    <p> Moku kurti statinį turinį. </p>
   </body>
 </html>
