d7648e25ecec22a1391340f88a2dbd2e873e3af6
steroMK <Tautvydas.Strelkauskas@gmail.com>
2019-10-29 16:04:34 +0200

    Praktika su php in html

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Aš valdau HTML!</title>
+    <title>Aš valdau HTML! <?php print ('ir PHP!'); ?></title>
   </head>
   <body>
-    <h1><b><i>Vardas</b></i> - HTML asas/ė!</h1>
-    <p> Moku kurti statinį turinį. </p>
+    <h1><b><i>Tautvydas</b></i> - HTML <?php print ('ir PHP '); ?> asas/ė!</h1>
+  <p> Moku kurti dinaminį turinį. </p>
+  <p> Atidaryta: <?php print(' Data: '); print(date("Y/m/d")); print(' Laikas: '); print(date("h:i:sa"));?>
   </body>
 </html>
