6681fa7e6c68839a7144892f4a38b8edbcfee84a
steroMK <Tautvydas.Strelkauskas@gmail.com>
2019-10-29 15:13:57 +0200

    PHP_Baze

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 print'Veikia!';
 ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>As valdau HTML!</title>
+  </head>
+  <body>
+    <h1><b><i>Vardas</b></i> - HTML asas/e!</h1>
+    <p> Moku kurti statini turini. </p>
+
+  </body>
+</html>
