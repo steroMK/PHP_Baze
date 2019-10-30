2987dcc4f8ad17ebb75d179437771273fde30fb5
steroMK <Tautvydas.Strelkauskas@gmail.com>
2019-10-30 15:59:47 +0200

    Praktika su CSS ir PHP

--- index.php
+++ index.php
@@ -2,10 +2,34 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>PHP lydės ir <?php print date('y', strtotime('+' . rand(1, 10) . ' years'));?> ! </title>
+        <title></title>
+        <style>
+            .dice_style {
+                height: 69vh;
+                width: 55vw;
+            } 
+            .dice_1 {
+                background-image: url("https://cdn0.iconfinder.com/data/icons/universal-flat/614/1011_-_Dice_One-512.png"); 
+            }
+            .dice_2 {
+                background-image: url("http://www.clipartpanda.com/clipart_images/red-dice-die-roll-two-square-47124751/download");
+            }
+            .dice_3 {
+                background-image: url("http://images.clipartpanda.com/3-rolling-dice-red_dice_die_roll_three_square_seal_stickers-r5338225e0b3a4a2596886a073889fd17_v9wf3_8byvr_512.jpg"); 
+            }
+            .dice_4 {
+                background-image: url("http://www.clipartpanda.com/clipart_images/red-dice-die-roll-four-square-47125719/download"); 
+            }
+            .dice_5 {
+                background-image: url("https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_5-512.png");  
+            }
+            .dice_6 {
+                background-image: url("https://rlv.zcache.com/red_dice_die_roll_six_square_seal-rde847a56866c495291424b11cf4a7ee4_0ugmc_8byvr_540.jpg");  
+            }
+        </style>
     </head>
     <body>
-        <h1><b>Tautvydas</b> - Galbūt turėsiu <?php print rand(1, 5);?> vaikų(us)!</h1> 
-        <p>D. Trump'as nebebus prezidentu: <?php print date('y/m/d', strtotime('+' . rand(2, 10) . ' years'));?></p> 
+        <div class="dice_<?php print rand(1, 6); ?> dice_style">
+        </div>
     </body>
 </html>
